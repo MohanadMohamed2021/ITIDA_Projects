10f5566cc1422df40ff08a186d7a917182790b96
Mohanad_Mohamed <mohanadmohamed20210@gmail.com>
2023-11-04 22:41:22 +0200

    project 2 using cmake

---
+++ snake_effect2/build/CMakeFiles/snake.dir/Src/main.c.o.d
@@ -0,0 +1,8 @@
+CMakeFiles/snake.dir/Src/main.c.o: \
+ /home/mohanad/Documents/test/snake_effect2/Src/main.c \
+ /home/mohanad/Documents/test/snake_effect2/Inc/bit.h \
+ /home/mohanad/Documents/test/snake_effect2/Inc/type.h \
+ /home/mohanad/Documents/test/snake_effect2/Lib/interface.h \
+ /usr/lib/avr/include/util/delay.h /usr/lib/avr/include/inttypes.h \
+ /usr/lib/gcc/avr/5.4.0/include/stdint.h /usr/lib/avr/include/stdint.h \
+ /usr/lib/avr/include/util/delay_basic.h /usr/lib/avr/include/math.h
