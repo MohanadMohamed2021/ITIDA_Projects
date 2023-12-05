383dda8f477b6f290ccb9132c78792909d2a6040
mohanad <mohanadmohamed20210@gmail.com>
2023-12-05 04:06:52 +0200

    Deleting

--- Led_Patterns/Leds/Lib/KPD/KPD_private.h
+++
@@ -1,9 +0,0 @@
-/******* Author  : Mohamed Hamdy *******/
-/******* Date    : 21/9/2022     *******/
-/******* SWC     : KPD           *******/
-/******* Version : 1             *******/
-
-#ifndef KPD_PRIVATE_H
-#define KPD_PRIVATE_H
-
-#endif
--- snake_effect2/build/CMakeFiles/DIO_programe.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for DIO_programe.
-# This may be replaced when dependencies are built.
