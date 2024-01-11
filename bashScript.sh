3f57bc189983675b0a6117424f589b87002b4124
MohanadMohamed2021 <mohanadmohamed20210@gmail.com>
2024-01-11 20:05:31 +0200

    d

---
+++ bashScript.sh
@@ -0,0 +1,16 @@
+#i/bin/bash
+
+git fetch origin ITIDA/Final-Project
+
+git chechout ITIDA/Final-Project
+
+rm DataBase.json Transaction_DataBase.json
+cp ~/server_json/DataBase.json  .
+cp ~/server_json/Transaction_DataBase.json  .
+
+git add DataBase.json Transaction_DataBase.json
+
+git commit -m "Updated Data-Base"
+
+git push origin ITIDA/Final-Project
+
